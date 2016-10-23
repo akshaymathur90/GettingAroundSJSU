736bbd453ea086ff620edd12f36826d58ade47b8
Akshay Mathur <akshay.mathur@sjsu.edu>
2016-10-23 16:33:06 -0700

    search

--- app/src/main/java/edu/sjsu/gettingaroundsjsu/MainActivity.java
+++ app/src/main/java/edu/sjsu/gettingaroundsjsu/MainActivity.java
@@ -1,13 +1,70 @@
 package edu.sjsu.gettingaroundsjsu;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG =getClass().getName().toString();
+
+    BuildingDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = new BuildingDatabase(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        // Inflate menu to add items to action bar if it is present.
+        inflater.inflate(R.menu.menu_main, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(getApplicationContext().SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+    public void onNewIntent(Intent intent) {
+        Log.d(TAG,"new Intent called");
+
+        setIntent(intent);
+        handleIntent(intent);
+    }
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query =
+                    intent.getStringExtra(SearchManager.QUERY);
+            doSearch(query);
+        }
+    }
+
+    private void doSearch(String queryStr) {
+        // get a Cursor, prepare the ListAdapter
+        // and set it
+        Log.d(TAG,"the search query is-->" + queryStr);
+        Cursor c = db.getWordMatches(queryStr, null);
+        if(c!=null && c.getCount()>0){
+            Log.d(TAG,"Val-->"+c.getString(0));
+            Log.d(TAG,"Val-->"+c.getString(1));
+
+        }
+        else{
+            Log.d(TAG,"No results found");
+        }
+
     }
 }
